bf12f7152e1e2795aacd9aefc904f3201cdd64aa
kalaiyarasi <kalai021998@gmail.com>
2025-08-06 21:41:32 +0530

    get prodcuct id

--- src/main/java/com/ec/product_service/controller/ProductController.java
+++ src/main/java/com/ec/product_service/controller/ProductController.java
@@ -3,6 +3,7 @@ package com.ec.product_service.controller;
 import com.ec.product_service.entity.Product;
 import com.ec.product_service.repo.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -26,5 +27,12 @@ public class ProductController {
     public List<Product> getAllProduct(){
         return productRepository.findAll();
 
+    }
+
+    @GetMapping("/{productId}")
+    public ResponseEntity<Product> getProductById(@PathVariable Long productId){
+        Product product = productRepository.findById(productId)
+                .orElseThrow(() -> new RuntimeException("Product not found with Id: "+ productId));
+        return ResponseEntity.ok(product);
     }
 }
